a9c6db8ae1ba07575cbf8c22a6b6c2cec52a430c
AnnieSld <anne.saldarriag@utec.edu.pe>
2025-10-23 09:00:55 -0500

    cambios

--- habits-svc/src/main.ts
+++ habits-svc/src/main.ts
@@ -4,25 +4,27 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  
-  // --- Configuración de prefijo global ---
-  app.setGlobalPrefix('habits');
 
-  // --- Configuración de Swagger ---
+  // ⚙️ Configuración Swagger antes del prefijo global
   const config = new DocumentBuilder()
     .setTitle('habits-svc')
     .setDescription('API de hábitos de estudiantes')
     .setVersion('1.0.0')
     .build();
-  const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('swagger-ui.html', app, document);
 
-  // --- ✅ Configuración de CORS ---
+  const document = SwaggerModule.createDocument(app, config);
+
+  // 👉 Montamos Swagger antes del prefijo global
+  SwaggerModule.setup('swagger', app, document);
+
+  // 🧩 Luego agregamos el prefijo global
+  app.setGlobalPrefix('habits');
+
   app.enableCors({
     origin: ['*'],
-    methods: '*',         // Permite todos los métodos (GET, POST, etc.)
-    allowedHeaders: '*',  // Permite todos los headers
-    credentials: true     // Necesario si usas cookies o auth
+    methods: '*',
+    allowedHeaders: '*',
+    credentials: true,
   });
 
   await app.listen(8083, '0.0.0.0');
